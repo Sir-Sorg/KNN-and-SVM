29aca16dae21c6379eee2571b17a5499e90a0bb1
Sir-Sorg <sinaorojlo53@gmail.com>
2022-12-08 19:10:21 +0330

    numpy add into project.

--- Holy_Ones.py
+++ Holy_Ones.py
@@ -1,8 +1,9 @@
-from numpy.random import multivariate_normal
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 from sklearn import metrics
-from random import shuffle, seed
 
 
 def addLable(list_: list, lable: int):
@@ -35,32 +36,33 @@ def testing(KNN, data):
     return accuracy
 
 
-CLASS_1_MEAN = [2.5, 0.25]
-CLASS_1_COVARIANCE = [[1, 0.75], [0.5, 1]]
-CLASS_2_MEAN = [-1.5, 0]
-CLASS_2_COVARIANCE = [[1, 0.5], [0.5, 1.5]]
+CLASS_1_MEAN = np.array([2.5, 0.25])
+CLASS_1_COVARIANCE = np.array([[1, 0.75], [0.5, 1]])
+CLASS_2_MEAN = np.array([-1.5, 0])
+CLASS_2_COVARIANCE = np.array([[1, 0.5], [0.5, 1.5]])
 
 # creating models
-C1_models = multivariate_normal(
+C1_models = np.random.multivariate_normal(
     CLASS_1_MEAN, CLASS_1_COVARIANCE, 70)
-C2_models = multivariate_normal(
+C1_lable = np.full((70, 1), 0)
+C1_models = np.append(C1_models, C1_lable, axis=1)
+C2_models = np.random.multivariate_normal(
     CLASS_2_MEAN, CLASS_2_COVARIANCE, 300)
+C2_lable = np.full((300, 1), 1)
+C2_models = np.append(C2_models, C2_lable, axis=1)
 
 # normalizing datas
-C1_models = standardization(C1_models).tolist()
-C2_models = standardization(C2_models).tolist()
+wholeData = np.vstack((C1_models, C2_models))
 
-# adding class lable to datas
-C1_models = addLable(C1_models, 0)
-C2_models = addLable(C2_models, 1)
-data = C1_models+C2_models
-seed(12)
-shuffle(data)
+# adding class lable to data
+data = np.concatenate((C1_models, C2_models))
+data_lable = C1_lable+C2_lable
 
 # learn percent = 70% and test = 30% --> lentgh * 0.3 and lentgh * 0.7 !!!
-trainTestBorder = round(len(data)*0.7)
-trainingData = data[0:trainTestBorder]
-testingData = data[trainTestBorder:]
+X_train, X_test, y_train, y_test = train_test_split(
+    data, data_lable, test_size=0.3, random_state=4)
+print('Train set:', X_train,  y_train)
+print('Test set:', X_test,  y_test)
 
 knn = train(trainingData)
 f_score = testing(knn, testingData)
